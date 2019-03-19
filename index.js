fba53ad91f0e68d26bb0622238abf4d92a6b65a3
Vinod Patidar <pvinod813@gmail.com>
2019-03-19 16:28:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function happyHolidayTo(holiday,name){
 }
 
 function holidayCountdown(days,holiday){
-  return `It's ${days} days until ${holiday}!`
+  return `It's ${holiday} days until ${days}!`
 }
 
 happyHolidayTo();
